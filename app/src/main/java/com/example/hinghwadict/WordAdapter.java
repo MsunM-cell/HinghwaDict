66643c9ae33e5838dbebb6a43d06755300d2864a
Spider <2464831035@qq.com>
2022-05-25 00:05:43 +0800

    style: format some adapter code

--- app/src/main/java/com/example/hinghwadict/WordAdapter.java
+++ app/src/main/java/com/example/hinghwadict/WordAdapter.java
@@ -10,18 +10,12 @@ import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
 
-
 import java.util.List;
 
 public class WordAdapter extends RecyclerView.Adapter<WordAdapter.MyViewHolder> {
     private List<SearchWordResponse.word> mDataset;
 
-    // Provide a reference to the views for each data item
-    // Complex data items may need more than one view per item, and
-    // you provide access to all the views for a data item in a view holder
     public static class MyViewHolder extends RecyclerView.ViewHolder {
-        // each data item is just a string in this case
-
         public TextView word;
         public TextView pinyin;
         public TextView ipa;
@@ -40,23 +34,17 @@ public class WordAdapter extends RecyclerView.Adapter<WordAdapter.MyViewHolder> 
         mDataset = myDataset;
     }
 
-    // Create new views (invoked by the layout manager)
     @Override
     public WordAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent,
                                                        int viewType) {
-        // create a new view
         View v = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.word_item, parent, false);
         MyViewHolder vh = new MyViewHolder(v);
         return vh;
     }
 
-    // Replace the contents of a view (invoked by the layout manager)
     @Override
     public void onBindViewHolder(MyViewHolder holder, int position) {
-        Log.d("dataset", String.valueOf(position));
-        // - get element from your dataset at this position
-        // - replace the contents of the view with that element
         holder.word.setText(mDataset.get(position).word);
         holder.pinyin.setText(mDataset.get(position).standard_pinyin);
         holder.ipa.setText("/" + mDataset.get(position).standard_ipa + "/");
@@ -65,17 +53,12 @@ public class WordAdapter extends RecyclerView.Adapter<WordAdapter.MyViewHolder> 
         holder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                Log.d("click", String.valueOf(holder.getAdapterPosition()));
                 int position = holder.getAdapterPosition();
                 SearchWordResponse.word w = mDataset.get(position);
-                Log.d("word", String.valueOf(w));
-//                Intent intent = new Intent(v.getContext(), SearchActivity.class);
-//                v.getContext().startActivity(intent);
             }
         });
     }
 
-    // Return the size of your dataset (invoked by the layout manager)
     @Override
     public int getItemCount() {
         return mDataset == null ? 0 : mDataset.size();
